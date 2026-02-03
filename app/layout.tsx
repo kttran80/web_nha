64ef133f8fedc808253d62fe6a8dc7f6472f4edc
kttran80 <tranthekiet@yahoo.com>
2026-02-03 10:39:25 -0600

    switch to system fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,7 @@
 import type { Metadata } from "next";
 import Link from "next/link";
-import { Geist, Geist_Mono } from "next/font/google";
 import ThemeToggle from "@/app/components/ThemeToggle";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Web nha",
@@ -26,9 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
         <header
           style={{
             position: "sticky",
